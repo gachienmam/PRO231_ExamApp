e5092d28d520b9128ac72895e1ff69d8befc774a
Pham Hong Khoa <phamhongkhoa911@gmail.com>
2025-03-12 23:22:04 +0700

    thêm quản lý người dùng

---
+++ ManagementApp/Formnguoidung.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ManagementApp
+{
+    partial class Formnguoidung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dataGridView1 = new DataGridView();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F);
+            label1.Location = new Point(67, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Mã:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(67, 192);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Vai trò:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F);
+            label3.Location = new Point(67, 144);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Họ và tên:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 12F);
+            label4.Location = new Point(441, 98);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 19);
+            label4.TabIndex = 3;
+            label4.Text = "Email:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Blue;
+            label5.Location = new Point(276, 24);
+            label5.Name = "label5";
+            label5.Size = new Size(246, 31);
+            label5.TabIndex = 4;
+            label5.Text = "Quản lý người dùng";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 12F);
+            label6.Location = new Point(441, 144);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 19);
+            label6.TabIndex = 5;
+            label6.Text = "Mật khẩu:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(116, 173);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(189, 48);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 18);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(61, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Admin";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(93, 18);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(74, 19);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Giáo viên";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(106, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(248, 22);
+            textBox1.TabIndex = 7;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(517, 141);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(210, 22);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(144, 141);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(210, 22);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(492, 95);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(235, 22);
+            textBox4.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 227);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(797, 208);
+            dataGridView1.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(209, 453);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(248, 22);
+            textBox5.TabIndex = 12;
+            textBox5.Text = "Tìm kiếm theo email";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(492, 453);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 13;
+            button1.Text = "Tìm kiếm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(92, 501);
+            button2.Name = "button2";
+            button2.Size = new Size(82, 50);
+            button2.TabIndex = 14;
+            button2.Text = "Thêm";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(233, 501);
+            button3.Name = "button3";
+            button3.Size = new Size(82, 50);
+            button3.TabIndex = 15;
+            button3.Text = "Sửa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(375, 501);
+            button4.Name = "button4";
+            button4.Size = new Size(82, 50);
+            button4.TabIndex = 16;
+            button4.Text = "Xóa";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(679, 501);
+            button5.Name = "button5";
+            button5.Size = new Size(82, 50);
+            button5.TabIndex = 17;
+            button5.Text = "Thoát";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(529, 501);
+            button6.Name = "button6";
+            button6.Size = new Size(82, 50);
+            button6.TabIndex = 18;
+            button6.Text = "Danh sách";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Formnguoidung
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 578);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(groupBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Formnguoidung";
+            Text = "Formnguoidung";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DataGridView dataGridView1;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ ManagementApp/Formnguoidung.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagementApp
+{
+    public partial class Formnguoidung : Form
+    {
+        public Formnguoidung()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

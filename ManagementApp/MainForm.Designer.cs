a244b914bfe611acf8e0c234922bc28460f47762
Pham Hong Khoa <phamhongkhoa911@gmail.com>
2025-03-12 21:49:52 +0700

    them label

--- ManagementApp/MainForm.Designer.cs
+++ ManagementApp/MainForm.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(297, 142);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Name = "MainForm";
             Text = "Quản lý bài thi - PolyTest";
+            TransparencyKey = Color.Fuchsia;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }

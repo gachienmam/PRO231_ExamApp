8efae4040728bf1a23ad397ec829a529b2b50129
gachienmam <thanhdatcaodang@gmail.com>
2025-03-13 13:09:38 +0700

    Changed project names

--- ManagementApp/MainForm.cs
+++ ManagementApp/MainForm.cs
@@ -2,6 +2,16 @@ namespace ManagementApp
 {
     public partial class MainForm : Form
     {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+
         public MainForm()
         {
             InitializeComponent();

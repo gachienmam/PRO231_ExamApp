5a91d44cc39d9c24ccd6c0c6b2d052355f0dfaf0
gachienmam <thanhdatcaodang@gmail.com>
2025-03-18 00:37:08 +0700

    Update database and fix build issues

--- ManagementApp/DangNhapForm.cs
+++ ManagementApp/DangNhapForm.cs
@@ -1,13 +1,24 @@
-﻿using ReaLTaiizor.Controls;
-using static ExamServer.AdminService.AdminService;
+﻿using ExamServer;
+using ReaLTaiizor.Controls;
+using System.Net;
+using static ExamServer.AdminService;
 
 namespace ManagementApp
 {
     public partial class DangNhapForm : Form
     {
+        private readonly AdminServiceClient _client;
+        private readonly string _serverAddress;
+
         public DangNhapForm()
         {
             InitializeComponent();
+        }
+
+        public DangNhapForm(string serverAddress)
+        {
+            InitializeComponent();
+            _serverAddress = serverAddress;
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
@@ -17,13 +28,13 @@ namespace ManagementApp
 
             try
             {
-                var request = new AdminServiceClient.AuthRequest { Email = email, Password = password };
+                var request = new AuthRequest { Email = email, Password = password };
                 var response = _client.AuthenticateUser(request);
 
-                if (response.IsAuthenticated)
+                if (response.ResponseCode == (int)HttpStatusCode.OK)
                 {
                     // Authentication successful, open the main management form
-                    MainForm mainForm = new MainForm(_serverAddress); // Pass the server address
+                    MainForm mainForm = new MainForm(_serverAddress, response.AccessToken); // Pass the server address
                     this.Hide();
                     mainForm.ShowDialog();
                     this.Close(); // Close the login form after the main form is closed

d055aa6776dc5a3bc6d843b2a4298630a6c06644
Ph1lipVu <vuthientruong013@gmail.com>
2025-03-19 23:53:23 +0700

    cap nhat tiep formgioithieu

--- ManagementApp/GioiThieuForm.Designer.cs
+++ ManagementApp/GioiThieuForm.Designer.cs
@@ -46,35 +46,35 @@
             crownSectionPanel1.Location = new Point(0, 0);
             crownSectionPanel1.Name = "crownSectionPanel1";
             crownSectionPanel1.SectionHeader = null;
-            crownSectionPanel1.Size = new Size(784, 380);
+            crownSectionPanel1.Size = new Size(870, 418);
             crownSectionPanel1.TabIndex = 0;
             // 
             // crownTitle4
             // 
             crownTitle4.AutoSize = true;
             crownTitle4.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            crownTitle4.Location = new Point(104, 247);
+            crownTitle4.Location = new Point(71, 255);
             crownTitle4.Name = "crownTitle4";
-            crownTitle4.Size = new Size(568, 13);
+            crownTitle4.Size = new Size(880, 13);
             crownTitle4.TabIndex = 6;
-            crownTitle4.Text = "Hệ thống cung cấp đầy đủ các chức năng như tạo đề thị, chấm điểm tự động và hiển thị kết quả ngay lập tức";
+            crownTitle4.Text = "Phần mềm cho phép tạo đề thi, quản lý ngân hàng câu hỏi, tổ chức thi và chấm điểm tự động, giúp giảm tải công việc cho giáo viên và nâng cao trải nghiệm người dùng.";
             // 
             // crownTitle1
             // 
             crownTitle1.AutoSize = true;
             crownTitle1.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            crownTitle1.Location = new Point(104, 223);
+            crownTitle1.Location = new Point(162, 213);
             crownTitle1.Name = "crownTitle1";
-            crownTitle1.Size = new Size(638, 13);
+            crownTitle1.Size = new Size(696, 13);
             crownTitle1.TabIndex = 5;
-            crownTitle1.Text = "Phần mềm thi trắc nghiệm là một ứng dụng hỗ trợ tổ chức và thực hiện các bài kiểm tra trắc nghiệm trực tuyến hoặc ofline.";
+            crownTitle1.Text = "EOS (Exam Online System) là một phần mềm thi trắc nghiệm trực tuyến, giúp tổ chức và quản lý các kỳ thi một cách dễ dàng, hiệu quả. ";
             crownTitle1.Click += crownTitle1_Click_1;
             // 
             // crownTitle3
             // 
             crownTitle3.AutoSize = true;
             crownTitle3.Font = new Font("Segoe UI Light", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            crownTitle3.Location = new Point(220, 95);
+            crownTitle3.Location = new Point(286, 73);
             crownTitle3.Name = "crownTitle3";
             crownTitle3.Size = new Size(277, 21);
             crownTitle3.TabIndex = 3;
@@ -84,7 +84,7 @@
             // 
             crownTitle2.AutoSize = true;
             crownTitle2.Font = new Font("Segoe UI Light", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            crownTitle2.Location = new Point(220, 145);
+            crownTitle2.Location = new Point(262, 127);
             crownTitle2.Name = "crownTitle2";
             crownTitle2.Size = new Size(349, 21);
             crownTitle2.TabIndex = 1;
@@ -94,7 +94,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 380);
+            ClientSize = new Size(870, 418);
             Controls.Add(crownSectionPanel1);
             MinimumSize = new Size(800, 419);
             Name = "GioiThieuForm";

6fd09de573d1e0b902906241cfc52f36a343e60b
Pham Hong Khoa <phamhongkhoa911@gmail.com>
2025-03-23 22:29:46 +0700

    them dal, bus Thi sinh

---
+++ ExamServer/Database/DAL/ThiSinh.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamServer.Database.DAL
+{
+    internal class ThiSinh
+    {
+        private readonly DatabaseHelper _dbHelper;
+
+        public ThiSinh(DatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public DataTable GetUserByUsername(string username)
+        {
+            string query = "SELECT * FROM ThiSinh WHERE MaThiSing = @MaThiSing";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@MaThiSing", username)
+            };
+
+            return _dbHelper.ExecuteQuery(query, parameters);
+        }
+    }
+}
